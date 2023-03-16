0e738c8acd1995a4768b6473867f9dcc9aebdbd2
Gadzeman <ostap778800@gmail.com>
2023-03-16 10:59:38 -0400

    ws subscribe message; working on code scturcure

--- src/chat/gateways/message.gateway.ts
+++ src/chat/gateways/message.gateway.ts
@@ -1,34 +1,54 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from "@nestjs/websockets";
 import { Server } from "socket.io";
-import { Subject } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import { MessageEntity } from "../entities/message.entity";
+import { MessageService } from "../services/message.service";
+
+enum EventNames {
+  MessageEventName = "message-event",
+}
 
 interface MessageEvent {
-  event: "message-event";
+  event: EventNames.MessageEventName;
   data: MessageEntity;
 }
 
 @WebSocketGateway(3001)
-export class MessageGateway implements OnGatewayDisconnect, OnGatewayConnection {
-  @WebSocketServer()
-  server: Server;
+export class MessageGateway
+  implements OnGatewayDisconnect, OnGatewayConnection
+{
+  constructor(private messageService: MessageService) {
+    this.messageService.$message.subscribe((message: MessageEntity) => {
+      this.$$messageEvent.next({
+        event: EventNames.MessageEventName,
+        data: message,
+      });
+    });
+  }
 
+  @WebSocketServer()
+  private server: Server;
+  private $$messageEvent = new Subject<MessageEvent>();
   private websockets = new Map<WebSocket, Subject<MessageEvent>>();
 
+  @SubscribeMessage(EventNames.MessageEventName)
+  handleMessageEvent(): Observable<MessageEvent> {
+    return this.$$messageEvent.asObservable();
+  }
+
   handleDisconnect(ws: WebSocket): void {
-    console.log("user disconnected");
     this.websockets.get(ws).complete();
     this.websockets.get(ws).unsubscribe();
     this.websockets.delete(ws);
   }
 
   handleConnection(ws: WebSocket): void {
-    console.log("user connected");
     this.websockets.set(ws, new Subject<MessageEvent>());
   }
 }
--- src/chat/services/message.service.ts
+++ src/chat/services/message.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { MessageEntity } from "../entities/message.entity";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class MessageService {
@@ -10,12 +11,16 @@ export class MessageService {
     private readonly repositoryMessage: Repository<MessageEntity>
   ) {}
 
+  private $$message = new Subject<Partial<MessageEntity>>();
+  public $message = this.$$message.asObservable();
+
   public async createMessage(message: MessageEntity): Promise<MessageEntity> {
     const createdMessage = await this.repositoryMessage.create({
       ...message,
       datetime: new Date(),
     });
     await this.repositoryMessage.save(createdMessage);
+    this.$$message.next(message);
     return createdMessage;
   }
 }
