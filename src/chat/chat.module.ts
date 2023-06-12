7b2dc2680104d2d4c02e8996a2eb2b715ee41604
Gadzeman <ostap778800@gmail.com>
2023-06-12 12:45:10 +0300

    message functionality to separeted message module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,17 +1,13 @@
 import { Module } from "@nestjs/common";
-import { MessageController } from "./controllers/message.controller";
-import { MessageService } from "./services/message.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { MessageEntity } from "./entities/message.entity";
-import { MessageGateway } from "./gateways/message.gateway";
 import { ChatController } from "./controllers/chat.controller";
 import { ChatEntity } from "./entities/chat.entity";
 import { ChatService } from "./services/chat.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MessageEntity, ChatEntity])],
-  controllers: [MessageController, ChatController],
-  providers: [MessageService, ChatService, MessageGateway],
-  exports: [MessageService, ChatService],
+  imports: [TypeOrmModule.forFeature([ChatEntity])],
+  controllers: [ChatController],
+  providers: [ChatService],
+  exports: [ChatService],
 })
 export class ChatModule {}
---
+++ src/message/message.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { MessageController } from "./controllers/message.controller";
+import { MessageService } from "./services/message.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { MessageEntity } from "./entities/message.entity";
+import { MessageGateway } from "./gateways/message.gateway";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([MessageEntity])],
+  controllers: [MessageController],
+  providers: [MessageService, MessageGateway],
+  exports: [MessageService],
+})
+export class ChatModule {}
